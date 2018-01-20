bc186f5482971ffc88aa9102c7f4244d6dd80807
Jens Nehlmeier <jens.nehlmeier@gmail.com>
2018-01-20 16:01:42 +0100

    Delete StyleInjectorImpl and move its methods to StyleInjector

--- src/main/java/com/google/gwt/dom/client/StyleInjector.java
+++ src/main/java/com/google/gwt/dom/client/StyleInjector.java
@@ -15,7 +15,6 @@
  */
 package com.google.gwt.dom.client;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.core.client.Scheduler;
@@ -26,65 +25,10 @@ import com.google.gwt.core.client.Scheduler.ScheduledCommand;
  * {@link #inject}, {@link #injectAtEnd}, and {@link #injectAtStart} use
  * {@link Scheduler#scheduleFinally} to minimize the number of individual style
  * elements created.
- * <p>
- * The api here is a bit redundant, with similarly named methods returning
- * either <code>void</code> or {@link StyleElement} &mdash; e.g.,
- * {@link #inject(String) void inject(String)} v.
- * {@link #injectStylesheet(String) StyleElement injectStylesheet(String)}. The
- * methods that return {@link StyleElement} are not guaranteed to work as
- * expected on Internet Explorer. Because they are still useful to developers on
- * other browsers they are not deprecated, but <strong>IE developers should
- * avoid the methods with {@link StyleElement} return values</strong> (at least
- * up until, and excluding, IE10).
  */
 public class StyleInjector {
 
-  /**
-   * The DOM-compatible way of adding stylesheets. This implementation requires
-   * the host HTML page to have a head element defined.
-   */
-  public static class StyleInjectorImpl {
-    private static final StyleInjectorImpl IMPL = new StyleInjectorImpl();
-
-    private HeadElement head;
-
-    public StyleElement injectStyleSheet(String contents) {
-      StyleElement style = createElement(contents);
-      getHead().appendChild(style);
-      return style;
-    }
-
-    public StyleElement injectStyleSheetAtEnd(String contents) {
-      return injectStyleSheet(contents);
-    }
-
-    public StyleElement injectStyleSheetAtStart(String contents) {
-      StyleElement style = createElement(contents);
-      getHead().insertBefore(style, head.getFirstChild());
-      return style;
-    }
-
-    public void setContents(StyleElement style, String contents) {
-      style.setInnerText(contents);
-    }
-
-    private StyleElement createElement(String contents) {
-      StyleElement style = Document.get().createStyleElement();
-      style.setPropertyString("language", "text/css");
-      setContents(style, contents);
-      return style;
-    }
-
-    private HeadElement getHead() {
-      if (head == null) {
-        Element elt = Document.get().getElementsByTagName("head").getItem(0);
-        assert elt != null : "The host HTML page does not have a <head> element"
-            + " which is required by StyleInjector";
-        head = HeadElement.as(elt);
-      }
-      return head;
-    }
-  }
+  private static HeadElement head;
 
   private static final JsArrayString toInject = JavaScriptObject.createArray().cast();
   private static final JsArrayString toInjectAtEnd = JavaScriptObject.createArray().cast();
@@ -188,15 +132,9 @@ public class StyleInjector {
 
   /**
    * Add a stylesheet to the document.
-   * <p>
-   * The returned StyleElement cannot be implemented consistently across all
-   * browsers. Specifically, <strong>applications that need to run on Internet
-   * Explorer should not use this method. Call {@link #inject(String)}
-   * instead.</strong>
-   * 
+   *
    * @param contents the CSS contents of the stylesheet
-   * @return the StyleElement that contains the newly-injected CSS (unreliable
-   *         on Internet Explorer)
+   * @return the StyleElement that contains the newly-injected CSS
    */
   public static StyleElement injectStylesheet(String contents) {
     toInject.push(contents);
@@ -206,15 +144,9 @@ public class StyleInjector {
   /**
    * Add stylesheet data to the document as though it were declared after all
    * stylesheets previously created by {@link #injectStylesheet(String)}.
-   * <p>
-   * The returned StyleElement cannot be implemented consistently across all
-   * browsers. Specifically, <strong>applications that need to run on Internet
-   * Explorer should not use this method. Call {@link #injectAtEnd(String)}
-   * instead.</strong>
    * 
    * @param contents the CSS contents of the stylesheet
-   * @return the StyleElement that contains the newly-injected CSS (unreliable
-   *         on Internet Explorer)
+   * @return the StyleElement that contains the newly-injected CSS
    */
   public static StyleElement injectStylesheetAtEnd(String contents) {
     toInjectAtEnd.push(contents);
@@ -224,15 +156,9 @@ public class StyleInjector {
   /**
    * Add stylesheet data to the document as though it were declared before any
    * stylesheet previously created by {@link #injectStylesheet(String)}.
-   * <p>
-   * The returned StyleElement cannot be implemented consistently across all
-   * browsers. Specifically, <strong>applications that need to run on Internet
-   * Explorer should not use this method. Call {@link #injectAtStart(String, boolean)}
-   * instead.</strong>
    * 
    * @param contents the CSS contents of the stylesheet
-   * @return the StyleElement that contains the newly-injected CSS (unreliable
-   *         on Internet Explorer)
+   * @return the StyleElement that contains the newly-injected CSS
    */
   public static StyleElement injectStylesheetAtStart(String contents) {
     toInjectAtStart.unshift(contents);
@@ -243,21 +169,17 @@ public class StyleInjector {
    * Replace the contents of a previously-injected stylesheet. Updating the
    * stylesheet in-place is typically more efficient than removing a
    * previously-created element and adding a new one.
-   * <p>
-   * This method should be used with some caution as StyleInjector may recycle
-   * StyleElements on certain browsers. Specifically, <strong>applications that
-   * need to run on Internet Explorer should not use this method. </strong>
    * 
    * @param style a StyleElement previously-returned from
    *          {@link #injectStylesheet(String)}.
    * @param contents the new contents of the stylesheet.
    */
   public static void setContents(StyleElement style, String contents) {
-    StyleInjectorImpl.IMPL.setContents(style, contents);
+    style.setInnerText(contents);
   }
 
   /**
-   * The <code>which</code> parameter is used to support the deprecated API.
+   * The <code>which</code> parameter is used to support the injectStylesheet API.
    */
   private static StyleElement flush(JavaScriptObject which) {
     StyleElement toReturn = null;
@@ -265,7 +187,7 @@ public class StyleInjector {
 
     if (toInjectAtStart.length() != 0) {
       String css = toInjectAtStart.join("");
-      maybeReturn = StyleInjectorImpl.IMPL.injectStyleSheetAtStart(css);
+      maybeReturn = injectStyleSheetAtStart(css);
       if (toInjectAtStart == which) {
         toReturn = maybeReturn;
       }
@@ -274,7 +196,7 @@ public class StyleInjector {
 
     if (toInject.length() != 0) {
       String css = toInject.join("");
-      maybeReturn = StyleInjectorImpl.IMPL.injectStyleSheet(css);
+      maybeReturn = injectStyleSheet(css);
       if (toInject == which) {
         toReturn = maybeReturn;
       }
@@ -283,7 +205,7 @@ public class StyleInjector {
 
     if (toInjectAtEnd.length() != 0) {
       String css = toInjectAtEnd.join("");
-      maybeReturn = StyleInjectorImpl.IMPL.injectStyleSheetAtEnd(css);
+      maybeReturn = injectStyleSheetAtEnd(css);
       if (toInjectAtEnd == which) {
         toReturn = maybeReturn;
       }
@@ -307,6 +229,39 @@ public class StyleInjector {
       needsInjection = true;
       Scheduler.get().scheduleFinally(flusher);
     }
+  }
+
+  private static StyleElement injectStyleSheet(String contents) {
+    StyleElement style = createElement(contents);
+    getHead().appendChild(style);
+    return style;
+  }
+
+  private static StyleElement injectStyleSheetAtEnd(String contents) {
+    return injectStyleSheet(contents);
+  }
+
+  private static StyleElement injectStyleSheetAtStart(String contents) {
+    StyleElement style = createElement(contents);
+    getHead().insertBefore(style, head.getFirstChild());
+    return style;
+  }
+
+  private static StyleElement createElement(String contents) {
+    StyleElement style = Document.get().createStyleElement();
+    style.setPropertyString("language", "text/css");
+    style.setInnerText(contents);
+    return style;
+  }
+
+  private static HeadElement getHead() {
+    if (head == null) {
+      Element elt = Document.get().getHead();
+      assert elt != null : "The host HTML page does not have a <head> element"
+          + " which is required by StyleInjector";
+      head = HeadElement.as(elt);
+    }
+    return head;
   }
 
   /**
